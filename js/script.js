c8d4cf1907efd7f3b902b7aa2abd0917f66e865d
“Andrew” <wjboff@hotmail.co.uk>
2026-01-12 14:39:13 +0000

    finalthree

--- js/script.js
+++ js/script.js
@@ -58,7 +58,7 @@ function getRandomColor() {
   const red = Math.floor(Math.random() * 256);
   const green = Math.floor(Math.random() * 256);
   const blue = Math.floor(Math.random() * 256);
-  // @return the random color value.
+  // return the random color value.
   return `
     rgb(${red}, ${green}, ${blue})
   `
